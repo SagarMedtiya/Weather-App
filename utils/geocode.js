9526304fbc86b8e4db6023d4f38d171fd3ff6e0c
Sagar Medtiya <sagarmedtiya@gmail.com>
2024-05-16 23:49:54 +0530

    geocode.js

---
+++ utils/geocode.js
@@ -0,0 +1,22 @@
+const request = require('request')
+
+const geocode = (address, callback)=>{
+    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) +'.json?access_token=pk.eyJ1IjoicHJha2FzaDkwMiIsImEiOiJjbHZ4cXVsNHUyaG40MnFtZzA2NzIzajIzIn0.8Xz9q3jn_YYhC9lohwUUCg&limit=1'
+    request({url: url, json: true },(error, response)=>{
+        if(error){
+            console.log('Unable to connect', undefined)
+        }else if(response.body.features.length === 0){
+            callback('Unable to find location. Try another search', undefined)
+        }
+        else{
+            callback(undefined, {
+                latitude : response.body.features[0].center[1],
+                longitude : response.body.features[0].center[0],
+                location : response.body.features[0].place_name
+            })
+        }
+
+    })
+}
+
+module.exports = geocode
