130a9ac9aa976fd048d087a6a3e08e209a8e226c
Sagar Medtiya <sagarmedtiya@gmail.com>
2024-05-20 23:53:55 +0530

    forecast.js

--- utils/forecast.js
+++ utils/forecast.js
@@ -2,16 +2,16 @@ const request = require('request')
 
 const forecast =(latitude,longitude, callback)=>{
     const url = 'http://api.weatherstack.com/current?access_key=e9c2820eb129d21b55e8bfb2b68eeed5&query='+latitude+','+longitude+'&units=f'
-    request({url:url, json:true},(error, response)=>{
+    request({ url , json:true },(error, {body})=>{
         if(error){
             callback('Unable to connect to weather service', undefined)
-        }else if(response.body.error){
+        }else if(body.error){
             callback('Unable to find location',undefined)
         }
         else{
             callback(undefined,{
-                Temperature: response.body.current.temperature,
-                Preciptation: response.body.current.precip
+                Temperature: body.current.temperature,
+                Preciptation: body.current.precip
             })
         }
     })
