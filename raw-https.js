3511f437b7d3ad9d5b7a393df18a34f1f053805f
Sagar Medtiya <sagarmedtiya@gmail.com>
2024-05-20 23:53:45 +0530

    raw.js

---
+++ raw-https.js
@@ -0,0 +1,7 @@
+const http = require('http');
+
+const url ='http://api.weatherstack.com/current?access_key=e9c2820eb129d21b55e8bfb2b68eeed5&query=40,-75&units=f'
+
+http.request(url,(response)=>{
+    reop
+})
