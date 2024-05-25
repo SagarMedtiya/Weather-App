f44990f66062820dc63790f6b6e863ab08568de4
Sagar Medtiya <sagarmedtiya@gmail.com>
2024-05-25 17:46:22 +0530

    app.js

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,5 +1,23 @@
 const express = require('express')
+const path = require('path')
 
+console.log(__dirname)
+console.log(__filename)
 const app = express()
 
-app.get()
+app.get('',(req,res)=>{
+    res.send('Hello express')
+})
+
+app.get('/help',(req,res)=>{
+    res.send({
+        name: 'Andrew',
+        age: 27
+    })
+})
+
+
+
+app.listen(3000,()=>{
+    console.log('Server is up on port 3000')
+})
