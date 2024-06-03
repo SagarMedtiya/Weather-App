064051692cc95571c5b4e01f358b2686347080bf
Sagar Medtiya <sagarmedtiya@gmail.com>
2024-06-03 20:34:28 +0530

    update app.js

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,22 +1,38 @@
 const express = require('express')
 const path = require('path')
-
-console.log(__dirname)
-console.log(__filename)
+const hbs = require('hbs')
 const app = express()
 
-app.get('',(req,res)=>{
-    res.send('Hello express')
-})
+//define paths for express config
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialPath = path.join(__dirname,'../templates/partials')
 
-app.get('/help',(req,res)=>{
-    res.send({
-        name: 'Andrew',
-        age: 27
+//setup handlebars engine and views location
+app.set('view engine','hbs')
+app.set('views',viewsPath)
+hbs.registerPartials(partialPath)
+
+app.use(express.static(path.join(__dirname, '../public')))
+app.get('',(req,res)=>{
+    res.render('index',{
+        title:'weather app',
+        name : 'sagar'
     })
 })
 
-
+app.get('/about',(req,res)=>{
+    res.render('about',{
+        title: 'about page',
+        name: 'sagar medtiya'
+    })
+})
+app.get('/help',(req,res)=>{
+    res.render('help',{
+        helpText: 'This is some helpful text',
+        title: 'Help',
+        name: 'Sagar'
+    })
+})
 
 app.listen(3000,()=>{
     console.log('Server is up on port 3000')
