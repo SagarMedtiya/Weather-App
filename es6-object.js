f639b8cdecaa965fe040b34daa6c921e7a56c211
Sagar Medtiya <sagarmedtiya@gmail.com>
2024-06-21 23:42:28 +0530

    es6-object

--- es6-object.js
+++ es6-object.js
@@ -1,25 +1,25 @@
-// object property shorthand
+// // object property shorthand
 
-const name = 'Sagar'
-const userAge = 25
+// const name = 'Sagar'
+// const userAge = 25
 
-const user ={
-    name: name,
-    age: userAge,
-    location:"Philadelphia"
-}
+// const user ={
+//     name: name,
+//     age: userAge,
+//     location:"Philadelphia"
+// }
 
-console.log(user)
+// console.log(user)
 
-const product ={
-    label : 'Red notebook',
-    price: 3,
-    stock: 201,
-    saleprice: undefined
-}
+// const product ={
+//     label : 'Red notebook',
+//     price: 3,
+//     stock: 201,
+//     saleprice: undefined
+// }
 
 
-const transaction =(type, {label, stock})=>{
+const transaction =(type, {label, stock}={})=>{
     console.log(type, label, stock)
 }
-transaction('order', product)
+transaction('order')
